96a6ce16ffd631708be280e99f08ef56a5885d94
Ivan Grahov <thebedtnoob.1957@gmail.com>
2025-03-27 10:22:08 +0200

    update config
    -

--- config.php
+++ config.php
@@ -1,15 +1,17 @@
 <?php
-$host = 'localhost';
-$dbname = 'unispace';
-include('../dbpass.php');
-// сложи в горния файл
-$username = ''; // Замени с твоя MySQL потребител
-$password = ''; // Замени с парола, ако има
+$host = 'localhost';      // Хост за базата данни (често localhost)
+$dbname = 'unispace';     // Името на базата данни
+
+// Задай директно потребителското име и парола тук
+$username = 'твое_потребителско_име';  // Замени с твоето MySQL потребителско име
+$password = 'твоята_парола';            // Замени с твоята MySQL парола
+
+// Опит за свързване с базата данни чрез PDO
 try {
     $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $username, $password);
-    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);  // Активиране на обработка на грешки
+    echo "Успешно свързан с базата данни!";
 } catch (PDOException $e) {
-    die("Грешка при връзка с базата: " . $e->getMessage());
+    die("Грешка при връзка с базата: " . $e->getMessage()); // Показва грешка, ако не може да се свърже
 }
 ?>
- 
